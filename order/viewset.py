acc4020825c33279e50766011eb2ca5da349c3d2
Ayu <aranajan@arkaenergy.com>
2025-03-27 12:08:45 +0530

    Added the data

--- order/viewset.py
+++ order/viewset.py
@@ -6,14 +6,24 @@ from rest_framework.authentication import TokenAuthentication
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     
+    def get_queryset(self):
+        user = self.request.user
+        organisation = user.organisation
+        table_id = self.request.query_params.get('table_id', None)
+        
+        if table_id:
+            return Order.objects.filter(table_id=table_id, organisation=organisation)
+        return Order.objects.filter(organisation=organisation)
+    
     
 class TableViewSet(viewsets.ModelViewSet):
-    queryset = Table.objects.all()
     serializer_class = TableSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Table.objects.filter(organisation=self.request.user.organisation)
